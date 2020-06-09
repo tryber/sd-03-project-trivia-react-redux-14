b4ee88d37da3ac253cc81d6c35254fb788703b38
ilan274 <ilan.herbach@gmail.com>
2020-06-09 14:45:15 -0300

    CC order issue on index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux/store';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
