5c35d7ea05bf48efbb8cda7acbb7131f0575463f
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-06-10 00:37:25 -0400

    fix bug md5 hash

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import CryptoJS from 'crypto-js';
+import MD5 from 'crypto-js/md5';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-
 
 export class Header extends Component {
   render() {
     const { name, email } = this.props;
-    const hash = CryptoJS.MD5(email);
+    const hash = MD5(email);
+    const gravatarIMG = `https://www.gravatar.com/avatar/${hash.toString()
+      .toLowerCase()}.jpg`;
     return (
       <div>
-        <p data-testid="header-score">0</p>
-        <p data-testid="header-player-name">{name}</p>
         <img
           data-testid="header-profile-picture"
           alt="Profile gravatar"
-          src={`https://www.gravatar.com/avatar/${hash}`}
+          src={gravatarIMG}
         />
+        <p data-testid="header-player-name">Jogador :{name}</p>
+        <p data-testid="header-score">Score :</p>
       </div>
     );
   }
@@ -31,6 +32,5 @@ Header.propTypes = {
   email: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
-
 
 export default connect(mapStateToProps, null)(Header);
