092a88b314d9c5d5bf0bcb150b48c2598085ef0a
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-06-10 10:19:19 -0300

    corecting code climate

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -14,18 +14,18 @@ class Questions extends Component {
   render() {
     const { questionsCategory } =this.props;
     return (
-      <div className="questions">
-        {categoryAndQuestion(questionsCategory, "category", "question-category")}
-        {categoryAndQuestion(questionsCategory, "question", "question-text")}
-          {questionsCategory === undefined ? 'carregando...' : questionsCategory.map(el => 
-            <button data-testid="correct-answer">
-              {el.correct_answer}
-            </button>)[0]}
-          {questionsCategory === undefined ? 'carregando...' : questionsCategory.map(el =>
-            el.incorrect_answers.map((el,index) => 
-            <button data-testid={`wrong-answer-${index}`}>
-              {el}
-            </button>))[0]}
+      <div className='questions'>
+        {categoryAndQuestion(questionsCategory, 'category', 'question-category')}
+        {categoryAndQuestion(questionsCategory, 'question', 'question-text')}
+        {questionsCategory === undefined ? 'carregando...' : questionsCategory.map((el) => 
+          <button data-testid='correct-answer'>
+            {el.correct_answer}
+          </button>)[0]}
+        {questionsCategory === undefined ? 'carregando...' : questionsCategory.map((el) =>
+          el.incorrect_answers.map((el,index) => 
+          <button data-testid={`wrong-answer-${index}`}>
+            {el}
+          </button>))[0]}
       </div>
     );
   }
@@ -37,10 +37,6 @@ const mapStateToProps = (state) => ({
 
 Questions.propTypes = {
   questionsCategory: PropTypes.func,
-};
-
-Questions.defaultProps = {
-  questionsCategory: '',
 };
 
 export default connect(mapStateToProps)(Questions);
