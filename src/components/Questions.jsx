94f94821585c953f21112b533d76763aa06064a5
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-06-10 10:29:48 -0300

    code climate again

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,29 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const categoryAndQuestion = (questionsCategory, object, idTest) => {
-  return(
+function categoryAndQuestion(questionsCategory, object, idTest) {
+  return (
     <p data-testid={idTest}>
-      {questionsCategory === undefined ? 'carregando...' : questionsCategory.map(el=>el[object])[0]}
+      {questionsCategory === undefined ?
+        'carregando...' : questionsCategory.map((categoryOrQuest) => categoryOrQuest[object])[0]}
     </p>
-  )
-}
+  );
+};
 
 class Questions extends Component {
   render() {
-    const { questionsCategory } =this.props;
+    const { questionsCategory } = this.props;
     return (
-      <div className='questions'>
+      <div className="questions">
         {categoryAndQuestion(questionsCategory, 'category', 'question-category')}
         {categoryAndQuestion(questionsCategory, 'question', 'question-text')}
-        {questionsCategory === undefined ? 'carregando...' : questionsCategory.map((el) => 
-          <button data-testid='correct-answer'>
-            {el.correct_answer}
+        {questionsCategory === undefined ? 'carregando...' : questionsCategory.map((correctAnswer) =>
+          <button data-testid="correct-answer">
+            {correctAnswer.correct_answer}
           </button>)[0]}
         {questionsCategory === undefined ? 'carregando...' : questionsCategory.map((el) =>
-          el.incorrect_answers.map((el,index) => 
+          el.incorrect_answers.map((incorrectAnswer, index) =>
           <button data-testid={`wrong-answer-${index}`}>
-            {el}
+            {incorrectAnswer}
           </button>))[0]}
       </div>
     );
