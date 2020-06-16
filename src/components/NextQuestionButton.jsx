88261575e88b928f3002f25d897ba6ddc4f8dd73
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-06-16 01:58:50 -0300

    testing code-climate

--- src/components/NextQuestionButton.jsx
+++ src/components/NextQuestionButton.jsx
@@ -10,9 +10,10 @@ class NextQuestionButton extends Component {
       <button
         data-testid="btn-next"
         onClick={() => {
-            if(questionNumber >= 4) return location.assign('/feedback')
-            return rehabilitateTimer(questionNumber)
-          }}>
+          if (questionNumber >= 4) return location.assign('/feedback')
+          return rehabilitateTimer(questionNumber);
+          }
+        }>
           Próxima
       </button>
     );
