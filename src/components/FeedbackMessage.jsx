cac4420845b2a325b3134fde7d2deedaf7c27926
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-06-16 04:11:23 -0300

    ranking-page

--- src/components/FeedbackMessage.jsx
+++ src/components/FeedbackMessage.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 export default class FeedbackMessage extends Component {
   render() {
-    const oldScore = JSON.parse(localStorage.getItem('state'));
+    const feedbackMessage = JSON.parse(localStorage.getItem('state'));
     return (
-      <div data-testid="feedback-text">
-        {oldScore.player.assertions < 3 && 'Podia ser melhor...'}
-        {oldScore.player.assertions >= 3 && 'Mandou bem!'}
+      <div data-testid="Message-text">
+        {feedbackMessage.player.assertions < 3 && 'Podia ser melhor...'}
+        {feedbackMessage.player.assertions >= 3 && 'Mandou bem!'}
       </div>
     );
   }
